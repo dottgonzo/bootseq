38265bb4bffb804379ccc6489adedea65e35556f
Dario Caruso <darioyzf@gmail.com>
2015-12-05 16:39:46 +0100

    to test

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var hostapdswitch=require('hostapd_switch'),
 Promise=require('promise'),
 testinternet=require('promise-test-connection'),
+PouchDB = require('pouchdb'),
 // npmAppsManager=require('npmAppsManager'),
 netw=require('netw'),
+mkdir=require('mkdir-p'),
 LMC=require('linux-mobile-connection'),
 mobileconnect=require('linux-mobile-connection'),
 pouchDB=require('pouchdb'),
@@ -15,20 +17,17 @@ _=require('lodash'),
 verb=require('verbo'),
 // cp = require('cp'),
 // configFile='./conf.json',
-app = require('express').createServer();
+express = require('express'),
+app     = express();
 
-var confhapds ={
 
+
+
+if(!pathExists.sync(__dirname+'/db/')){
+  mkdir.sync(__dirname+'/db/');
 }
-var hostapd_switch=new hostapdswitch(confhapds);
-
-
-// if(!pathExists.sync(configFile)){
-// cp.sync('./conf.example.json', configFile)
-// }
-// sudo iptables -t nat -A PREROUTING -d 0/0 -p tcp --dport 80 -j DNAT --to-destination 192.168.23.1 // better to custom port
-
-app.use('/db', require('express-pouchdb')(PouchDB.defaults({prefix: '/tmp/my-temp-pouch/'})));
+var pdb=PouchDB.defaults({prefix: __dirname+'/db/'});
+app.use('/db', require('express-pouchdb')(pdb));
 app.use(express.static(__dirname +'/html'));
 
 
@@ -36,36 +35,22 @@ app.get('/', function(req, res){
   res.send('hello world');
 });
 
-function regular_mode(options){
-
+function init_apps(options){
   return new Promise(function(resolve,reject){
-    hostapd_switch.client('/etc/wpa_supplicant/wpa_supplicant.conf',true,true).then(function(switch_answer){
-      resolve(switch_answer)
-      // npmAppsManager.start().then(function(answer){
-      //   resolve(answer)
-      // }).catch(function(err){
-      //   verb(err,'error','bootseq')
-      //   reject(err)
-      // })
-
-
-    }).catch(function(err){
-      reject(err)
-
-    })
+    console.log('starting apps');
+    resolve(true)
   })
+
 }
 
 function recovery_mode(options){
+  var apswitch=new hostapdswitch(confhapds);
   return new Promise(function(resolve,reject){
-    hostapd_switch.ap().then(function(){
+    apswitch.ap().then(function(){
       resolve(answer)
-
     }).catch(function(err){
       verb(err,'error','bootseq')
-
       reject(err)
-
     })
   })
 
@@ -76,62 +61,71 @@ function J5(data) {
   var config={
     recovery:true,
     offlineApp:false, // avvia l'app solo in stato regular
-    port:8080, // in modalità regular setta la porta per il manager
+    port:4000, // in modalità regular setta la porta per il manager
     wpa_supplicant_path:'/etc/wpa_supplicant/wpa_supplicant.conf',
     recovery_dev:'auto'
   }
 
   if(data){
-
     merge(config,data)
-
   }
 
+  app.listen(config.port);
 
-  app.listen(options.port);
-
-  this.j5=config
-}
+  this.config=config
+};
 
 J5.prototype.start_apps=function(){
-  return npmAppsManager.start()
-};
+  //  return npmAppsManager.start()
+},
 J5.prototype.stop_apps=function(){
-  return npmAppsManager.stop(this)
-};
-J5.prototype.restart_apps=function(){
+  //   return npmAppsManager.stop(this)
+},
+J5.prototype.wifi_switch=function(mode,dev){
 
-  return npmAppsManager.restart(this)
-};
-J5.prototype.app_mode_switch=function(mode){
-  switch(mode){
-    case 'recovery':{
-
-      return recovery_mode(this)
-    };
-    case 'regular':{
-
-      return regular_mode(this)
-    }
+  if(dev){
+    var apswitch=new hostapdswitch(
+      {
+        interface:dev,
+        hostapd:this.hostapd
+      }
+    );
+    return apswitch.mode(mode)
+  }else{
+    return new Promise(function(resolve,reject){
+      netw().then(function(data){
+        _.map(data.networks,function(device){
+          if(device.interfaceType=='wifi'){
+            dev=device.interface
+          }
+        })
+        if(dev){
+          var apswitch=new hostapdswitch(
+            {
+              interface:dev,
+              hostapd:this.hostapd
+            }
+          );
+          apswitch.mode(mode).then(function(answer){
+            resolve(answer)
+          }).catch(function(err){
+            reject(err)
+          })
+        }else{
+          reject(err)
+        }
+      }).catch(function(err){
+        reject(err)
+      })
+    })
   }
-})
-J5.prototype.wifi_switch=function(mode){
-  switch(mode){
-    case 'ap':{
-      return hostapd_switch.ap()
-    };
-    case 'host':{
-      return hostapd_switch.host()
-    };
-    case 'client':{
-      return hostapd_switch.client()
-    }
-  }
-})
+
+
+},
 
 
 J5.prototype.init=function(){
-  var options=this;
+  var config=this.config;
   return new Promise(function(resolve,reject){
     testinternet().then(function(){
       npmAppsManager.start().then(function(answer){
@@ -141,79 +135,76 @@ J5.prototype.init=function(){
         reject(err)
       })
     }).catch(function(){
+      verb(err,'info','Tryng to connect')
       var wifi_exist=false
-
       netw.data().then(function(data){
         _.map(data.networks,function(device){
           if(device.interfaceType=='wifi'){
             wifi_exist=true
           }
         })
-      })
-      if(wifi_exist){
-        hostapd_switch.client().then(function(answer){
-
-          regular_mode(options).then(function(answer){
-            resolve(answer)
-          }).catch(function(err){
-            reject(err)
-
-            verb(err,'error','bootseq')
-          })
-
-        }).catch(function(err){
-
-          if(mobile){
-            linuxmobile().then(function(){
-
-            }).catch(function(){
-              if(options.recovery){
-                recovery_mode(options).then(function(answer){
-                  resolve(answer)
-                }).catch(function(err){
-                  verb(err,'error','bootseq')
-                })
-              } else{
-                reject('no wlan host available')
-              }
-
-            })
-          }
-
-
-
-        })
-
-      } else{
-        if(mobile){
-          linuxmobile().then(function(){
-            regular_mode(options).then(function(answer){
+        if(wifi_exist){
+          verb(err,'info','Wlan interface founded');
+          var apswitch=new hostapdswitch(confhapds);
+          apswitch.client().then(function(answer){
+            init_apps(options).then(function(answer){
               resolve(answer)
             }).catch(function(err){
               reject(err)
-              verb(err,'error','bootseq')
+              verb(err,'error','J5 start apps')
             })
-
-          }).catch(function(){
-            reject({'no connections available'})
-
+          }).catch(function(err){
+            if(config.mobile){
+              var mobile=new linuxmobile(config.mobile)
+              linuxmobile.connect().then(function(){
+                init_apps(options).then(function(answer){
+                  resolve(answer)
+                }).catch(function(err){
+                  reject(err)
+                  verb(err,'error','J5 start apps')
+                })
+              }).catch(function(){
+                if(options.recovery){
+                  recovery_mode(options).then(function(answer){
+                    resolve(answer)
+                  }).catch(function(err){
+                    verb(err,'error','J5 recovery mode start')
+                    reject(err)
+                  })
+                } else{
+                  reject('no wlan host available')
+                }
+              })
+            }
+          })
+        } else{
+          if(config.mobile){
+            var mobile=new linuxmobile(config.mobile)
+            linuxmobile.connect().then(function(){
+              init_apps(options).then(function(answer){
+                resolve(answer)
+              }).catch(function(err){
+                reject(err)
+                verb(err,'error','J5 start apps')
+              })
+            }).catch(function(err){
+              verb(err,'error','J5 linuxmobile')
+              reject(err)
+            })
           }
-
-
         }
-
-
+      }).catch(function(err){
+        verb(err,'error','J5 NETW ERROR!!')
+        reject(err)
       })
-
-
-
     })
-  };
+  })
+},
 
-  J5.prototype.recovery=function(){
-    // return recovery_mode(this)
-  };
+J5.prototype.recovery=function(){
+  // return recovery_mode(this)
+};
 
 
 
-  module.export=J5
+module.exports=J5
